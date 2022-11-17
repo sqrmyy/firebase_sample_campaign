666cd7399aaadd4ab5b02eafa8dd9fb0681d1991
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-11-17 20:03:41 +0900

    Cloud Firestore からのデータ取得、表示

--- lib/main.dart
+++ lib/main.dart
@@ -52,20 +52,44 @@ class _MyWidgetState extends State<MyWidget> {
           children: [
             Expanded(
               child: Container(
-                height: double.infinity,
-                alignment: Alignment.topCenter,
-                child: ListView.builder(
-                  itemCount: reverseList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Center(
-                      child: Text(
-                        reverseList[index],
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                  height: double.infinity,
+                  alignment: Alignment.topCenter,
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('dream')
+                        .orderBy('createdAt')
+                        .snapshots(),
+                    builder: ((context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('エラーが発生しました');
+                      }
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      final list = snapshot.requireData.docs
+                          .map<String>((DocumentSnapshot document) {
+                        final documentData =
+                            document.data()! as Map<String, dynamic>;
+                        return documentData['content']! as String;
+                      }).toList();
+
+                      final reverseList = list.reversed.toList();
+
+                      return ListView.builder(
+                        itemCount: reverseList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Center(
+                            child: Text(
+                              reverseList[index],
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          );
+                        },
+                      );
+                    }),
+                  )),
             ),
             Row(
               children: [
